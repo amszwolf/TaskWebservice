dde4c384f638f1e1a203e0a711f80fc40aff8a10
hailong han <hailong.han@qq.com>
2022-01-17 16:43:05 +0800

    add dirrect run in linux

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
--- taskclient.js
+++ taskclient.js
@@ -1,4 +1,4 @@
-﻿//#!/usr/bin/env node
+﻿#!/usr/bin/env node
 
 var http = require('http');
 var program = require('commander');
